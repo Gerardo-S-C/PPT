7f8d2db8967ea5ecfba04b7f4120a3a93b608b29
GerardoS-C <gsandovalc1900@alumno.ipn.com>
2021-11-22 00:36:28 -0600

    ppt con node

---
+++ rock-paper-scissors/util/users.js
@@ -0,0 +1,23 @@
+const connectedUsers = {};
+const choices = {};
+const moves = {
+    "roca": "tijeras",
+    "papel": "roca",
+    "tijeras": "papel"
+};
+
+const initializeChoices = (roomId) => {
+    choices[roomId] = ["", ""]
+}
+
+const userConnected = (userId) => {
+    connectedUsers[userId] = true;
+}
+
+const makeMove = (roomId, player, choice) => {
+    if(choices[roomId]){
+        choices[roomId][player - 1] = choice;
+    }
+}
+
+module.exports = {connectedUsers, initializeChoices, userConnected, makeMove, moves, choices};
